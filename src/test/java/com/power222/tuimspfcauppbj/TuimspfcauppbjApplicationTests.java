5419a2a82e9eac5468ef83c5a701b0287a63bb01
Simon Longpré-Landry <simonllandry@hotmail.com>
2020-09-03 19:58:55 -0400

    EQ-35: Upload initial du projet Spring

---
+++ src/test/java/com/power222/tuimspfcauppbj/TuimspfcauppbjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.power222.tuimspfcauppbj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuimspfcauppbjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
