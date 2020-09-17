a9c8a24ca944e0d1f48c4b759958f65e872141ed
Ernesto231 <max2power222gmail.com>
2020-09-17 18:38:12 -0400

    EQ3-40 corriger les erreurs des tests

--- src/test/java/com/power222/tuimspfcauppbj/controllers/StudentControllerTests.java
+++ src/test/java/com/power222/tuimspfcauppbj/controllers/StudentControllerTests.java
@@ -4,7 +4,6 @@ import com.power222.tuimspfcauppbj.controller.StudentController;
 import com.power222.tuimspfcauppbj.dao.StudentRepository;
 import com.power222.tuimspfcauppbj.dao.UserRepository;
 import com.power222.tuimspfcauppbj.model.Student;
-import com.power222.tuimspfcauppbj.model.User;
 import com.power222.tuimspfcauppbj.service.AuthenticationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +12,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.awt.*;
-import java.util.Collections;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @WebMvcTest(StudentController.class)
 public class StudentControllerTests {
 
@@ -154,44 +153,41 @@ public class StudentControllerTests {
     @Test
     void createStudentSameUsernameTest() throws Exception{
         /**
-        Student s = Student.builder().enabled(true)
-                .username("etudiant")
-                .role("student")
-                .password(new BCryptPasswordEncoder().encode("password"))
-                .firstName("Bob")
-                .lastName("Brutus")
-                .id(4L)
-                .studentId("1234")
-                .email("power@gmail.ca")
-                .phoneNumber("911")
-                .address("9310 Lasalle")
-                .build();
+         Student s = Student.builder().enabled(true)
+         .username("etudiant")
+         .role("student")
+         .password(new BCryptPasswordEncoder().encode("password"))
+         .firstName("Bob")
+         .lastName("Brutus")
+         .id(4L)
+         .studentId("1234")
+         .email("power@gmail.ca")
+         .phoneNumber("911")
+         .address("9310 Lasalle")
+         .build();
 
-        studentRepository.saveAndFlush(s);
+         studentRepository.saveAndFlush(s);
 
-        Student s1 = Student.builder().enabled(true)
-                .username("etudiant")
-                .role("student")
-                .password(new BCryptPasswordEncoder().encode("password"))
-                .firstName("Bob")
-                .lastName("Brutus")
-                .id(5L)
-                .studentId("1234")
-                .email("power@gmail.ca")
-                .phoneNumber("911")
-                .address("9310 Lasalle")
-                .build();
+         Student s1 = Student.builder().enabled(true)
+         .username("etudiant")
+         .role("student")
+         .password(new BCryptPasswordEncoder().encode("password"))
+         .firstName("Bob")
+         .lastName("Brutus")
+         .id(5L)
+         .studentId("1234")
+         .email("power@gmail.ca")
+         .phoneNumber("911")
+         .address("9310 Lasalle")
+         .build();
 
-        when(studentRepository.saveAndFlush(any())).thenReturn(s1);
+         when(studentRepository.saveAndFlush(any())).thenReturn(s1);
 
-        mvc.perform(post("/students").contentType(MediaType.APPLICATION_JSON).content("{\"id\":5,\"username\":\"etudiant\",\"role\":\"student\",\"enabled\":true,\"firstName\":\"Bob\",\"lastName\":\"Brutus\",\"studentId\":\"1234\",\"email\":\"power@gmail.ca\",\"phoneNumber\":\"911\",\"address\":\"9310Lasalle\"}"))
-                .andExpect(status().isConflict());
+         mvc.perform(post("/students").contentType(MediaType.APPLICATION_JSON).content("{\"id\":5,\"username\":\"etudiant\",\"role\":\"student\",\"enabled\":true,\"firstName\":\"Bob\",\"lastName\":\"Brutus\",\"studentId\":\"1234\",\"email\":\"power@gmail.ca\",\"phoneNumber\":\"911\",\"address\":\"9310Lasalle\"}"))
+         .andExpect(status().isConflict());
          **/
 
-         }
-
-
-
+    }
 
 
 }
--- src/test/java/com/power222/tuimspfcauppbj/services/AuthenticationServiceTests.java
+++ src/test/java/com/power222/tuimspfcauppbj/services/AuthenticationServiceTests.java
@@ -4,27 +4,21 @@ import com.power222.tuimspfcauppbj.dao.UserRepository;
 import com.power222.tuimspfcauppbj.model.User;
 import com.power222.tuimspfcauppbj.service.AuthenticationService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
-
-import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
-
 public class AuthenticationServiceTests {
 
-    @Mock
+    @Autowired
     private UserRepository userRepo;
 
-    @MockBean
+    @Autowired
     private AuthenticationService authSvc;
 
     @Test
